63c25f5e70f65abf78218121179d8e73d825fcd4
Juan Antonio Soto C <renitope@gmail.com>
2024-03-02 23:19:09 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'local-notificaciones',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
